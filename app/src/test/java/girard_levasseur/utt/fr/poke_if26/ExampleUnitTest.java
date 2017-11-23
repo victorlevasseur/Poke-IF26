47aa8c3453eaf1d98d57fd2e197de7b437e9c1b2
Victor Levasseur <victorlevasseur52@gmail.com>
2017-11-23 14:14:19 +0100

    Add initial project

---
+++ app/src/test/java/girard_levasseur/utt/fr/poke_if26/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package girard_levasseur.utt.fr.poke_if26;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
